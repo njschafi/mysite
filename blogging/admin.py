09e4f6990b76c05affbe59eea17fd095fba00acf
njschafi <njschafi@gmail.com>
2019-08-14 08:17:16 -0700

    Lesson07 Assignment

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInline(admin.TabularInline):
+    model = Post.categories.through
+    #model = Post.categories.through
+
+class PostAdmin(admin.ModelAdmin):
+    empty_value_display = '-Empty Field-'
+    inlines = [CategoryInline,]
+
+class CategoryAdmin(admin.ModelAdmin):
+    empty_value_display = '-Empty Field-'
+    exclude = ('posts',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
